ef4a9377c5ac7910cc471f625066485fea1dc241
Artem <snegirevnews@gmail.com>
2019-04-09 20:11:03 +0300

    firstCommit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: 'login-component',
+  templateUrl: 'login.component.html',
+    styleUrls: ['login.component.scss'],
 })
-export class HomePage {
+export class LoginComponent {
 
 }
