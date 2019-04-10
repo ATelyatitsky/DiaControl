daecece5465f80bfa43ccf4c486df4b265c5b409
MacOs <snegirevnews@gmail.com>
2019-04-11 01:50:29 +0300

    Переработал страницу авторизации.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
 import {AutorisationService} from '../Services/autorisation.service';
+import {Keyboard} from '@ionic-native/keyboard';
 
 const routes: Routes = [
   {
@@ -23,7 +24,7 @@ const routes: Routes = [
     RouterModule.forChild(routes)
   ],
     providers: [
-        AutorisationService
+        AutorisationService,
     ],
   declarations: [LoginPage]
 })
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
 import {AutorisationService} from '../Services/autorisation.service';
+import {Keyboard} from '@ionic-native/keyboard/ngx';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +9,17 @@ import {AutorisationService} from '../Services/autorisation.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private autorisationService: AutorisationService) { }
-
+  constructor(private autorisationService: AutorisationService,
+              private keyboard: Keyboard) { }
+  public noBlur = true;
   ngOnInit() {
+      window.addEventListener('keyboardWillShow', data => {
+          this.noBlur = false;
+      });
+      window.addEventListener('keyboardWillHide', data => {
+          this.noBlur = true;
+      });
   }
-
     redirectToRegistrationPage(path: string): void {
         this.autorisationService.redirectTo(path);
     }
